205a43b121666b26c566ebc2f90ce3f873e63c4c
amaya lim <amayalim99@gmail.com>
2025-01-06 06:55:05 -0800

    major app update 01.03.25

--- app/app.py
+++ app/app.py
@@ -1,31 +1,35 @@
 from flask import Flask, session, redirect, request, url_for, render_template
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql.sqltypes import ARRAY
-from sqlalchemy import desc, cast, func
+from flask_migrate import Migrate
+from sqlalchemy import desc, cast, case
 import requests
 import os
 from dotenv import load_dotenv
-from urllib.parse import urlencode, quote
+from urllib.parse import urlencode
 from apscheduler.schedulers.background import BackgroundScheduler
 import re
 import time
 import model
+import graphs
 
 load_dotenv()
 
 client_secret = os.getenv('CLIENT_SECRET')
 client_id = os.getenv('CLIENT_ID')
-redirect_uri=os.getenv('REDIRECT_URI')
 scope = os.getenv('SCOPE')
 secret_key = 'aaaaaaaaaaaaaaaa'
+
+redirect_uri=os.getenv('REDIRECT_URI_PROD')
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///playlists.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.secret_key = secret_key
 db = SQLAlchemy(app)
-
-# app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
+migrate = Migrate(app, db)
 
 class Playlist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,11 +38,13 @@ class Playlist(db.Model):
     url = db.Column(db.String)
     image = db.Column(db.String)
 
+    tracks = db.relationship('Track', secondary='tracklist', back_populates='playlists')
+
     def __repr__(self):
         return f"<{self.name}>"
 
 class Track(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
     track_id = db.Column(db.String(50), nullable=False)
     name = db.Column(db.String(100), nullable=False)
     artist = db.Column(db.String(100), nullable=False)
@@ -53,10 +59,9 @@ class Track(db.Model):
     liveness = db.Column(db.Float)
     valence = db.Column(db.Float)
     tempo = db.Column(db.Float)
-    playlist_id = db.Column(db.Integer, db.ForeignKey('playlist.playlist_id'), nullable=False)
-    artist_id = db.Column(db.String, db.ForeignKey('artist.artist_id'))
+    artist_id = db.Column(db.String, db.ForeignKey('artist.artist_id'), nullable=False)
 
-    playlist = db.relationship('Playlist', backref='playlist.playlist_id')
+    playlists = db.relationship('Playlist', secondary='tracklist', back_populates='tracks')
 
     def __repr__(self):
         return f"<{self.name}>"
@@ -72,7 +77,11 @@ class Artist(db.Model):
     def __repr__(self):
         return f"<{self.name}"
 
-scheduler = BackgroundScheduler()
+tracklist = db.Table('tracklist',
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('track_id', db.String(50), db.ForeignKey('track.track_id')),
+    db.Column('playlist_id', db.String(50), db.ForeignKey('playlist.playlist_id'))
+)
 
 with app.app_context():
     # db.drop_all()
@@ -83,18 +92,6 @@ with app.app_context():
 def flatten(lst):
     return [item for sublist in lst for item in sublist]
 
-def playlist_tracks(playlist_id):
-    tracks = db.session.query(Track.name).filter_by(playlist_id=playlist_id).all()
-    return [track.name for track in tracks]
-
-def playlist_genres(playlist_id):
-    playlist_artists = db.session.query(Track.artist_id).filter_by(playlist_id=playlist_id).all()
-    genres = []
-    for artist in playlist_artists:
-        genre_list = db.session.query(Artist.genres).filter_by(artist_id=artist.artist_id).first().genres
-        genres.append((genre_list))
-    return flatten(genres)
-
 def model_playlists(playlist_id):
     recs = model.get_recs(playlist_id=playlist_id)
     model_playlists=[]
@@ -104,23 +101,48 @@ def model_playlists(playlist_id):
     sorted_playlists = sorted(model_playlists, key=lambda p: int(p.name.split()[0]), reverse=True)
     return sorted_playlists
 
-@app.route('/')
+def make_graphs(playlist_id, viewport):
+    playlist = Playlist.query.filter_by(playlist_id=playlist_id).first_or_404()
+    recs = model_playlists(playlist_id)
+    audio_graph = graphs.spider_graph(playlist, recs, viewport)
+    genre_graph = graphs.genres_graph(playlist, recs, viewport)
+    return audio_graph, genre_graph
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
     playlists = Playlist.query.order_by(desc(cast(Playlist.name, db.Integer))).all()
-    tracks = Track.query.all()
-
+    # tracks = Track.query.all()
     if playlists:
-        return render_template('index.html', playlist_tracks=playlist_tracks, playlist_genres=playlist_genres, playlists=playlists, tracks=tracks)
+        return render_template('index.html', playlists=playlists)
     else:
         return render_template('index.html', playlists=None, tracks=None)
     
-@app.route('/playlist/<string:playlist_id>')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+    
+@app.route('/playlist/<string:playlist_id>', methods=['POST', 'GET'])
 def playlist_details(playlist_id):
     playlist = Playlist.query.filter_by(playlist_id=playlist_id).first_or_404()
-    playlist_tracks = Track.query.filter_by(playlist_id=playlist_id).all()
+    track_ids = [
+        track_id[0]
+        for track_id in db.session.query(tracklist.c.track_id)
+        .filter_by(playlist_id=playlist_id)
+        .order_by(tracklist.c.id)
+        .all()
+    ]
+    order_cases = case(
+        *( (Track.track_id == track_id, index) for index, track_id in enumerate(track_ids) )
+    )
+    playlist_tracks = (
+        db.session.query(Track)
+        .filter(Track.track_id.in_(track_ids))
+        .order_by(order_cases)
+    )
     sorted_playlists = model_playlists(playlist_id)
-    print(model_playlists)
-    return render_template('playlist.html', playlist=playlist, playlist_tracks=playlist_tracks, model_playlists=sorted_playlists)
+    viewport = request.args.get('width', type=int)
+    graphs = make_graphs(playlist_id, viewport)
+    return render_template('playlist.html', playlist=playlist, playlist_tracks=playlist_tracks, model_playlists=sorted_playlists, graphs=graphs, viewport=viewport)
 
 @app.route('/artist/<string:artist_id>')
 def artist_playlists(artist_id):
@@ -128,7 +150,8 @@ def artist_playlists(artist_id):
     tracks = db.session.query(Track).filter_by(artist_id=artist_id).all()
     artist_playlists = []
     for track in tracks:
-        playlists = Playlist.query.filter_by(playlist_id=track.playlist_id).all()
+        playlist_id = db.session.query(tracklist.c.playlist_id).filter_by(track_id=track.track_id).first().playlist_id
+        playlists = db.session.query(Playlist).filter_by(playlist_id=playlist_id).all()
         artist_playlists.append(playlists)
     sorted_playlists = sorted(
         artist_playlists,
@@ -136,8 +159,6 @@ def artist_playlists(artist_id):
         reverse=True
     )
     return render_template('artist.html', artist_playlists=sorted_playlists, artist_name=artist_name)
-
-# @app.route('/recommendations/<string:playlist_id>')
 
 @app.route('/authorize')
 def authorize():
@@ -165,15 +186,12 @@ def callback():
         }
         response = requests.post(token_url, data=token_data)
         tokens = response.json()
-        print(tokens)
         access_token = tokens['access_token']
         refresh_token = tokens['refresh_token']
 
         session['access_token'] = access_token
         session['refresh_token'] = refresh_token
         session.modified = True
-
-        print('got here')
 
         updated = update()
 
@@ -182,7 +200,6 @@ def callback():
             return redirect(url_for('index'))
     return redirect(url_for('authorize'))       
 
-#refresh
 def refresh_access_token():
     url = "https://api.spotify.com/v1/me"
     access_token = session.get('access_token')
@@ -213,10 +230,21 @@ def batch(lst, n):
         else:
             yield lst[i:]
 
+def add_track_to_playlist(playlist_id, track_id):
+    playlist = Playlist.query.filter_by(playlist_id=playlist_id).first()
+    track = Track.query.filter_by(track_id=track_id).first()
+
+    exists = db.session.query(tracklist).filter_by(playlist_id=playlist_id, track_id=track_id).first()  
+    if playlist and track and exists==None:
+        playlist.tracks.append(track)
+    elif playlist:
+        print(f"Error on '{playlist.name}'")
+    db.session.commit()
+    return
+
 def get_playlists(num_playlists, limit=50):
     access_token = refresh_access_token()
     if access_token:
-        print('yes access token')
         headers = {
             'Authorization': f'Bearer {access_token}',
         }
@@ -251,11 +279,43 @@ def get_playlists(num_playlists, limit=50):
                         url=url,
                         image=image
                     )
-                    get_tracks(playlist_id)
                     db.session.add(new_playlist)
+                    db.session.commit()
+                    get_tracks(playlist_id)
                     new_playlist_count+=1
-        db.session.commit()
+        print(new_playlist_count)
         return new_playlist_count
+    return False
+
+@app.route('/get/<string:playlist_id>')
+def get_playlist(playlist_id):
+    access_token = refresh_access_token()
+    if access_token:
+        headers = {
+            'Authorization': f'Bearer {access_token}',
+        }
+
+        playlist_url = f'https://api.spotify.com/v1/playlists/{playlist_id}'
+        response = requests.get(playlist_url, headers=headers)
+        playlist = response.json()
+        name = playlist['name']
+        playlist_id = playlist['id']
+        url = playlist['external_urls']['spotify']
+        image = playlist['images'][0]['url']
+
+        existing_playlist = db.session.query(Playlist.playlist_id).filter_by(playlist_id=playlist_id).first()
+        image_reg = "^https:\/\/image-cdn-ak"
+        if not existing_playlist and re.match(image_reg, image) != None:
+            new_playlist = Playlist(
+                name=name,
+                playlist_id=playlist_id,
+                url=url,
+                image=image
+            )
+            db.session.add(new_playlist)
+            db.session.commit()
+        get_tracks(playlist_id)
+        return redirect(url_for('index'))
     return False
 
 def get_tracks(playlist_id):
@@ -282,48 +342,81 @@ def get_tracks(playlist_id):
                     track_id=track_id,
                     name=track_name,
                     artist=artist_name,
-                    playlist_id=playlist_id,
                     artist_id=artist_id
                 )
                 get_artists(artist_id)
                 db.session.add(new_track)
-        db.session.commit()
+                db.session.commit()
+            add_track_to_playlist(playlist_id, track_id)
     return
 
 
+# def get_audio_features(num_tracks):
+#     track_ids = db.session.query(Track.track_id).order_by(Track.track_id.desc()).limit(num_tracks).all()
+#     print('track ids', len(track_ids))
+#     access_token = refresh_access_token()
+#     if access_token:
+#         headers = {
+#             'Authorization': f'Bearer {access_token}',
+#         }
+
+#         batched = batch(track_ids, 100)
+#         for each in batched:
+#             track_ids_string = ','.join([str(item[0]) for item in each])
+#             af_url = f"https://api.spotify.com/v1/audio-features?ids={track_ids_string}"
+#             print(af_url)
+#             af_response = requests.get(af_url, headers=headers)
+#             af_data = af_response.json()['audio_features']
+
+#             for each_track in af_data:
+#                 reg = '(?<=track:).*'
+#                 spotify_id = re.findall(reg, each_track['uri'])[0]
+#                 track = db.session.query(Track).filter_by(track_id=spotify_id).first()
+
+#                 track.danceability = each_track['danceability']
+#                 track.energy = each_track['energy']
+#                 track.key = each_track['key']
+#                 track.loudness = each_track['loudness']
+#                 track.mode = each_track['mode']
+#                 track.speechiness = each_track['speechiness']
+#                 track.acousticness = each_track['acousticness']
+#                 track.instrumentalness = each_track['instrumentalness']
+#                 track.liveness = each_track['liveness']
+#                 track.valence = each_track['valence']
+#                 track.tempo = each_track['tempo']
+#         db.session.commit()
+#     return
+
 def get_audio_features(num_tracks):
-    track_ids = db.session.query(Track.track_id).limit(-num_tracks).all()
-    access_token = refresh_access_token()
-    if access_token:
-        print('yes access token')
-        headers = {
-            'Authorization': f'Bearer {access_token}',
-        }
+    track_ids = db.session.query(Track.track_id).order_by(Track.track_id.desc()).limit(num_tracks).all()
+    headers = {
+        'Accept': 'application/json',
+    }
 
-        batched = batch(track_ids, 100)
-        for each in batched:
-            track_ids_string = ','.join([item[0] for item in each])
-            af_url = f"https://api.spotify.com/v1/audio-features?ids={quote(track_ids_string)}"
-            af_response = requests.get(af_url, headers=headers)
-            af_data = af_response.json()['audio_features']
+    for each in track_ids:
+        track_id = each[0]
+        print('track_id', track_id)
+        af_url = f"https://soundlens.pro/api/spotify-replacement/audio-features/{track_id}"
+        af_metadata = requests.get(af_url, headers=headers).json()
+        af_response = requests.get(af_metadata['analysis_url'])
+        af_data = af_response.json()
+        print('response', af_data)
 
-            for each_track in af_data:
-                reg = '(?<=track:).*'
-                spotify_id = re.findall(reg, each_track['uri'])[0]
-                track = db.session.query(Track).filter_by(track_id=spotify_id).first()
+        spotify_id = af_data['id']
+        track = db.session.query(Track).filter_by(track_id=spotify_id).first()
 
-                track.danceability = each_track['danceability']
-                track.energy = each_track['energy']
-                track.key = each_track['key']
-                track.loudness = each_track['loudness']
-                track.mode = each_track['mode']
-                track.speechiness = each_track['speechiness']
-                track.acousticness = each_track['acousticness']
-                track.instrumentalness = each_track['instrumentalness']
-                track.liveness = each_track['liveness']
-                track.valence = each_track['valence']
-                track.tempo = each_track['tempo']
-        db.session.commit()
+        track.danceability = af_data['danceability']
+        track.energy = af_data['energy']
+        track.key = af_data['key']
+        track.loudness = af_data['loudness']
+        track.mode = af_data['mode']
+        track.speechiness = af_data['speechiness']
+        track.acousticness = af_data['acousticness']
+        track.instrumentalness = af_data['instrumentalness']
+        track.liveness = af_data['liveness']
+        track.valence = af_data['valence']
+        track.tempo = af_data['tempo']
+    db.session.commit()
     return
 
 def get_artists(artist_id):
@@ -350,16 +443,16 @@ def get_artists(artist_id):
 
 @app.route('/update')
 def update():
+    model.update_similarities()
     num_playlists = get_playlists(10, 10)
-    get_audio_features(num_playlists)
-    print('updated!', num_playlists)
-    return True
+    # get_audio_features(100)
+    return redirect(url_for('index'))
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(update, 'interval', days=1)
 scheduler.start()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
-    time.sleep(1)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
